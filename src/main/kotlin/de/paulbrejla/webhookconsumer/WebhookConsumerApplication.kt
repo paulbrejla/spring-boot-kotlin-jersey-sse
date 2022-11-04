c0409cf0bd75207a0c14208cf284ff6d62d5e6a3
Paul Brejla <paul.brejla@googlemail.com>
2022-11-04 13:58:38 +0100

    add actuator

--- src/main/kotlin/de/paulbrejla/webhookconsumer/WebhookConsumerApplication.kt
+++ src/main/kotlin/de/paulbrejla/webhookconsumer/WebhookConsumerApplication.kt
@@ -2,6 +2,7 @@ package de.paulbrejla.webhookconsumer
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.jms.annotation.EnableJms
 
 @SpringBootApplication
 class WebhookConsumerApplication
--- src/main/kotlin/de/paulbrejla/webhookconsumer/rest/impl/EventWsV1Impl.kt
+++ src/main/kotlin/de/paulbrejla/webhookconsumer/rest/impl/EventWsV1Impl.kt
@@ -2,26 +2,27 @@ package de.paulbrejla.webhookconsumer.rest.impl
 
 import de.paulbrejla.webhookconsumer.application.api.EventService
 import de.paulbrejla.webhookconsumer.rest.api.EventWsV1
-import org.glassfish.jersey.inject.hk2.RequestContext
-import org.json.JSONObject
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import javax.ws.rs.PathParam
 import javax.ws.rs.core.Context
 import javax.ws.rs.core.HttpHeaders
 import javax.ws.rs.core.Response
-import javax.ws.rs.core.UriInfo
 import javax.ws.rs.sse.Sse
 import javax.ws.rs.sse.SseEventSink
 
 @Component("eventWsV1")
 class EventWsV1Impl(val eventService: EventService) : EventWsV1 {
+    var logger: Logger = LoggerFactory.getLogger(this::class.java)
     override fun receivePayload(
         body: Any,
         @PathParam(value = "listenerId") listenerId: String,
         @Context context: HttpHeaders
     ): Response {
-
+        logger.info(body.toString())
         eventService.sendEvent(listenerId, body, context.requestHeaders)
+
         return Response.accepted().build()
     }
 
